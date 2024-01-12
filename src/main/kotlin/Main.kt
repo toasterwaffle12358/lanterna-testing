95be0a1dd75081fc1cb24083d8c160bc5812b098
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2024-01-12 12:57:31 -0800

    more learning, yay

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,15 @@ package org.example
 
 //imports
 import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.graphics.TextGraphics
+import com.googlecode.lanterna.graphics.Theme
+import com.googlecode.lanterna.graphics.ThemeStyle
 import com.googlecode.lanterna.gui2.*
 import com.googlecode.lanterna.screen.Screen
 import com.googlecode.lanterna.screen.TerminalScreen
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
 import com.googlecode.lanterna.terminal.Terminal
+import java.awt.Frame
 
 
 fun main() {
@@ -16,17 +20,25 @@ fun main() {
     val terminal: Terminal = DefaultTerminalFactory().createTerminal()
     val screen: Screen = TerminalScreen(terminal)
     screen.startScreen()
+    terminal.terminalSize.rows
 
     //making panel that holds components
     val panel: Panel = Panel()
-    panel.setLayoutManager(GridLayout(2))
+    panel.setPreferredSize(terminal.terminalSize.withRows(5))
+    panel.addComponent(Label("omg haiii :3"))/*.withBorder(Borders.doubleLineBevel("omgggggggg no wayyy ")))*/
+    panel.fillColorOverride = TextColor.RGB(173, 216, 230)
+
+
 
     // Create window to hold the panel
     val window = BasicWindow()
     window.component = panel
 
 
+
+
     // Create gui and start gui
-    val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.ANSI.BLUE))
+    val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.RGB(248, 200, 220)))
     gui.addWindowAndWait(window)
+
 }
