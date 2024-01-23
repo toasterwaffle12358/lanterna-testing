5f3ada5ecbb6d548cd2955f182e6711a64db8a63
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2024-01-23 12:20:27 -0800

    found out how to have multiple windows

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,11 @@ package org.example
 
 //imports
 import com.googlecode.lanterna.TextColor
-import com.googlecode.lanterna.graphics.TextGraphics
-import com.googlecode.lanterna.graphics.Theme
-import com.googlecode.lanterna.graphics.ThemeStyle
 import com.googlecode.lanterna.gui2.*
 import com.googlecode.lanterna.screen.Screen
 import com.googlecode.lanterna.screen.TerminalScreen
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
 import com.googlecode.lanterna.terminal.Terminal
-import java.awt.Frame
 
 
 fun main() {
@@ -23,16 +19,23 @@ fun main() {
     terminal.terminalSize.rows
 
     //making panel that holds components
-    val panel: Panel = Panel()
-    panel.setPreferredSize(terminal.terminalSize.withRows(5))
-    panel.addComponent(Label("omg haiii :3"))/*.withBorder(Borders.doubleLineBevel("omgggggggg no wayyy ")))*/
+    val panel: Panel = Panel().setLayoutManager(BorderLayout())
+    //panel.setPreferredSize(terminal.terminalSize.withRows(5))
+    panel.addComponent(Label("omg haiii :3").setBackgroundColor(TextColor.RGB(173, 216, 230)))
     panel.fillColorOverride = TextColor.RGB(173, 216, 230)
+    panel.setLayoutData(BorderLayout.Location.BOTTOM);
 
 
 
     // Create window to hold the panel
     val window = BasicWindow()
     window.component = panel
+    window.title = "no freaking way this is so crazy"
+    window.setHints(listOf(Window.Hint.CENTERED, Window.Hint.EXPANDED));
+    val window2 = BasicWindow()
+    window2.component = panel
+    window2.title = "hehe :3"
+    window2.setHints(listOf(Window.Hint.CENTERED, Window.Hint.EXPANDED));
 
 
 
@@ -40,5 +43,6 @@ fun main() {
     // Create gui and start gui
     val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.RGB(248, 200, 220)))
     gui.addWindowAndWait(window)
+    gui.addWindowAndWait(window2)
 
 }
