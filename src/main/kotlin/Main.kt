472f314ceeb5d9f4b9bd3b4504a099265db3e93a
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2024-01-23 13:40:53 -0800

    theming and multiple labels on the same window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package org.example
 
 //imports
 import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.graphics.SimpleTheme
 import com.googlecode.lanterna.gui2.*
 import com.googlecode.lanterna.screen.Screen
 import com.googlecode.lanterna.screen.TerminalScreen
@@ -12,6 +13,8 @@ import com.googlecode.lanterna.terminal.Terminal
 fun main() {
     println("Hello World!")
 
+    val basicTheme = SimpleTheme.makeTheme(false, TextColor.RGB( 235, 232, 200 ), TextColor.RGB(14, 21, 24), TextColor.RGB(14, 21, 24), TextColor.RGB(14, 21, 24), TextColor.RGB(14, 21, 24), TextColor.RGB(14, 21, 24), TextColor.RGB(14, 21, 24))
+
     //setting up terminal and screen layers
     val terminal: Terminal = DefaultTerminalFactory().createTerminal()
     val screen: Screen = TerminalScreen(terminal)
@@ -20,10 +23,10 @@ fun main() {
 
     //making panel that holds components
     val panel: Panel = Panel().setLayoutManager(BorderLayout())
-    //panel.setPreferredSize(terminal.terminalSize.withRows(5))
-    panel.addComponent(Label("omg haiii :3").setBackgroundColor(TextColor.RGB(173, 216, 230)))
-    panel.fillColorOverride = TextColor.RGB(173, 216, 230)
-    panel.setLayoutData(BorderLayout.Location.BOTTOM);
+    panel.addComponent(Label("omg haiii :3").setBackgroundColor(TextColor.RGB(22, 38, 46)).withBorder(Borders.doubleLine()).setLayoutData(BorderLayout.Location.LEFT))
+    panel.addComponent(Label("test test test").setBackgroundColor(TextColor.RGB(22, 38, 46)).withBorder(Borders.doubleLine()).setLayoutData(BorderLayout.Location.CENTER))
+    panel.addComponent(Label("dfsgkljdlfkgdslkfjlkdjsf").setBackgroundColor(TextColor.RGB(22, 38, 46)).withBorder(Borders.doubleLine()).setLayoutData(BorderLayout.Location.RIGHT))
+    panel.fillColorOverride = TextColor.RGB(22, 38, 46)
 
 
 
@@ -31,18 +34,15 @@ fun main() {
     val window = BasicWindow()
     window.component = panel
     window.title = "no freaking way this is so crazy"
-    window.setHints(listOf(Window.Hint.CENTERED, Window.Hint.EXPANDED));
-    val window2 = BasicWindow()
-    window2.component = panel
-    window2.title = "hehe :3"
-    window2.setHints(listOf(Window.Hint.CENTERED, Window.Hint.EXPANDED));
+    window.setHints(listOf(Window.Hint.CENTERED, Window.Hint.FULL_SCREEN, Window.Hint.NO_POST_RENDERING));
+    window.theme = basicTheme
+
 
 
 
 
     // Create gui and start gui
-    val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.RGB(248, 200, 220)))
+    val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.RGB(16, 22, 24)))
     gui.addWindowAndWait(window)
-    gui.addWindowAndWait(window2)
 
 }
